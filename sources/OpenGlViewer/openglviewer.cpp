2e7e0aebdd43f2570b51e8b57bfb860f86fdbbcd
TonyTone <erlinmaquer@gmail.com>
2020-08-04 00:56:11 +0300

    Add waiting cursor for operation Align_pair and merge.

--- sources/OpenGlViewer/openglviewer.cpp
+++ sources/OpenGlViewer/openglviewer.cpp
@@ -340,6 +340,7 @@ void OpenGlViewer::saveFirstMesh()
 
 void OpenGlViewer::alignSecondMesh()
 {
+     QApplication::setOverrideCursor(Qt::WaitCursor);
     if(drawFirstObject->fn==0 || drawSecondObject->fn==0)
     {
         QMessageBox::warning(this, "Warning","Please, choose two objects");
@@ -439,10 +440,12 @@ void OpenGlViewer::alignSecondMesh()
     else
         emit setDistanceInLabel(QString("Distance:\ndd="+QString::number(distance.first)+"\ndd="+QString::number(distance.second)));
     update();
+    QApplication::restoreOverrideCursor();
 }
 
 void OpenGlViewer::appendSecondMeshToFirst()
 {
+    QApplication::setOverrideCursor(Qt::WaitCursor);
     if((*drawFirstObject).fn==0 || (*drawSecondObject).fn==0)
     {
         QMessageBox::warning(this, "Warning extension","Please select two objects");
@@ -457,6 +460,7 @@ void OpenGlViewer::appendSecondMeshToFirst()
     (*drawSecondObject).vert.clear();
     emit setDistanceInLabel(QString(""));
     update();
+    QApplication::restoreOverrideCursor();
 }
 
 void OpenGlViewer::setShowGrid(bool value)
