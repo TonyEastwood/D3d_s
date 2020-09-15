0d5be26bb0016133a462b8d7aa66baf6e686ef48
Tony <erlinmaquer@gmail.com>
2020-09-15 19:52:49 +0300

    Change run program behavior: If amount of input parameters > 1, then run program in integration mode (but without setParent and integration in another window) Form shows maximized as default

--- main.cpp
+++ main.cpp
@@ -18,7 +18,7 @@ int main(int argc, char *argv[])
 
     QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalAppendMesh,&mainWin,&MainAppWindow::signalAppendMesh);
     QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalClearMeshesData,&mainWin,&MainAppWindow::signalClearMeshesData);
-    QObject::connect(&messageReceiver,&MainWindowForMessageReceive::appIntegrate,&mainWin,&MainAppWindow::appIntegrate);
+    //QObject::connect(&messageReceiver,&MainWindowForMessageReceive::appIntegrate,&mainWin,&MainAppWindow::appIntegrate);
     QObject::connect(&messageReceiver,&MainWindowForMessageReceive::appChangeSize,&mainWin,&MainAppWindow::appChangeSize);
     QObject::connect(&messageReceiver,&MainWindowForMessageReceive::appHide,&mainWin,&MainAppWindow::hide);
     QObject::connect(&messageReceiver,&MainWindowForMessageReceive::appShow,&mainWin,&MainAppWindow::show);
@@ -35,7 +35,7 @@ int main(int argc, char *argv[])
     //mainWin.setStyleSheet(" background-color:"+CONSTANTS::backgroundColor+";");
     mainWin.setContentsMargins(0,0,0,0);
 
-    if(argc==2)
+    if(argc>1)
     {
         //progress bar
         QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalSetQuantity,&mainWin,&MainAppWindow::setMaxValue);
@@ -43,27 +43,27 @@ int main(int argc, char *argv[])
         QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalShowProgressBar,&mainWin,&MainAppWindow::showProgressBar);
         QObject::connect(&mainWin,&MainAppWindow::signalCancelScanning,&messageReceiver,&MainWindowForMessageReceive::cancelScanning);
         mainWin.Initialize(true);
-        HWND hw=(HWND)std::stoull(argv[1]);
+        //HWND hw=(HWND)std::stoull(argv[1]);
         //HWND hw=(HWND)atoi(argv[1]);
-        emit messageReceiver.appIntegrate(hw);
+       // emit messageReceiver.appIntegrate(hw);
 
     }
-    else if(argc==4)
-    {
-        //progress bar
-        QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalSetQuantity,&mainWin,&MainAppWindow::setMaxValue);
-        QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalSetValue,&mainWin,&MainAppWindow::setCurrentValue);
-        QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalShowProgressBar,&mainWin,&MainAppWindow::showProgressBar);
-        QObject::connect(&mainWin,&MainAppWindow::signalCancelScanning,&messageReceiver,&MainWindowForMessageReceive::cancelScanning);
-        mainWin.Initialize(true);
-        HWND hw=(HWND)std::stoull(argv[1]);
-        int width=QString(argv[2]).toInt();
-        int height=QString(argv[3]).toInt();
-        messageReceiver.appIntegrate(hw);
-        emit messageReceiver.appChangeSize(0,0,width,height);
-    }
+//    else if(argc==4)
+//    {
+//        //progress bar
+//        QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalSetQuantity,&mainWin,&MainAppWindow::setMaxValue);
+//        QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalSetValue,&mainWin,&MainAppWindow::setCurrentValue);
+//        QObject::connect(&messageReceiver,&MainWindowForMessageReceive::signalShowProgressBar,&mainWin,&MainAppWindow::showProgressBar);
+//        QObject::connect(&mainWin,&MainAppWindow::signalCancelScanning,&messageReceiver,&MainWindowForMessageReceive::cancelScanning);
+//        mainWin.Initialize(true);
+//        HWND hw=(HWND)std::stoull(argv[1]);
+//        int width=QString(argv[2]).toInt();
+//        int height=QString(argv[3]).toInt();
+//        messageReceiver.appIntegrate(hw);
+//        emit messageReceiver.appChangeSize(0,0,width,height);
+//    }
     else  mainWin.Initialize(false);
-    mainWin.show();
+    mainWin.showMaximized();
 
 
 
